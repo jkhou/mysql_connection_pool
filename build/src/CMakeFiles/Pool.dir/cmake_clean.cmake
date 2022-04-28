9de3cb7aebce578795d38ffbc66b5e5de1267431
jkhou <cquhjk@126.com>
2022-04-28 09:25:36 +0800

    add codes

---
+++ build/src/CMakeFiles/Pool.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Pool.dir/connect.cpp.o"
+  "CMakeFiles/Pool.dir/connect_pool.cpp.o"
+  "../../lib/libPool.pdb"
+  "../../lib/libPool.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Pool.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
